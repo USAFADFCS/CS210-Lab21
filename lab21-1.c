2c9186530d5163816a11028a02e1f37b3b14289f
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-10-07 10:52:27 -0600

    Uploading Template with Required Files

--- lab21-1.c
+++ lab21-1.c
@@ -1,39 +1,16 @@
+/** lab21-1.c
+ * ===========================================================
+ * Name: First Last
+ * Section: xxx
+ * Project: Lab 21
+ * ===========================================================
+ */
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
 
-    FILE* file = NULL;
-    char inputFileName[100] = "";
+    return 0;
 
-    scanf("%s", inputFileName);
-
-    file = fopen(inputFileName, "r");
-
-    if (file == NULL) {
-        // Do Something.
-    }
-
-    char line[100] = "";
-    
-    do {
-        
-        fgets(line, 100, file);
-
-        int numCommas = 0;
-
-        for (int i=0; i<strlen(line); i++) {
-            if (line[i] == ',') {
-                numCommas++;
-            }
-
-            if (numCommas == 1 && line[i] != ',') {
-                printf("%c", line[i]);
-            }
-        }
-        
-        printf("\n");
-
-    } while (!feof(file));
 }
--- lab21-2.c
+++ lab21-2.c
@@ -1,53 +1,16 @@
+/** lab21-2.c
+ * ===========================================================
+ * Name: First Last
+ * Section: xxx
+ * Project: Lab 21
+ * ===========================================================
+ */
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
 
-    FILE* file = NULL;
-    char inputFileName[100] = "";
-
-    scanf("%s", inputFileName);
-
-    file = fopen(inputFileName, "r");
-
-    if (file == NULL) {
-        // Do Something.
-    }
-
-    char prevTime[50] = "";
-    char prevTitle[100] = "";
-
-    // Read File Contents
-    char time[50]; 
-    char title[100];
-    char rating[50];
-
-    int firstLine = 0;
+    return 0;
     
-    do {
-        
-        fscanf(file, "%[^,],%[^,],%[^\n]\n", time, title, rating);
-        
-        if (strlen(title) > 44) {
-            title[44] = '\0';
-        }
-
-        if (strcmp(title, prevTitle) == 0) {
-            printf(" %5s", time);
-        }
-        else {
-            if (firstLine == 0) {
-                firstLine++;
-            }
-            else {
-                printf("\n");
-            }
-            
-            printf("%-44s | %5s | %5s", title, rating, time);
-        }
-
-        strcpy(prevTitle, title);
-
-    } while (!feof(file));
 }
